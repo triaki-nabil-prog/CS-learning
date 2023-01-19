6a57c483304a852e843227e5f643280afe014897
triaki-nabil-prog <nabil.triaki2@gmail.com>
2023-01-19 22:55:03 +0100

    created fe more methods to do operation on the linked list for practice purposes

--- linkedList.js
+++ linkedList.js
@@ -13,15 +13,85 @@ function LinkedList() {
             tmp.next = node;
         }
     };
-// prepending a new node to the head of the link list
+    // prepending a new node to the head of the link list
     const prepend = function (value) {
         // create a new node with the value 
         let node = Node(value);
         node.next = this.head;
         this.head = node;
     }
+    // calculate number of nodes in the linked list
+    const size = function () {
+        let size = 0;
+        let tmp = this.head;
+        while (tmp !== null) {
+            tmp = tmp.next;
+            size++;
+        }
+        return size;
+    }
+    // returns the first node in the list
+    const findHead = function () {
+        return this.head.value;
+    }
+    // returns the last node in the list
+    const findTail = function () {
+        let tail = this.head;
+        while (tail.next !== null) { tail = tail.next }
+        return tail;
+    }
+    // returns the node at the given index 
+    const at = function (index) {
+        let tmp = this.head;
+        if (index < 0) return;
+        for (let i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp;
+    }
+    //remove the last element from the list
+    const pop = function () {
+        let tmp = this.head;
+        while (tmp.next.next !== null) { tmp = tmp.next; }
+        tmp.next = null;
+        return tmp;
+    }
+    //returns true if the passed in value is in the list 
+    const contains = function (value) {
+        let tmp = this.head;
+        while (tmp !== null) {
+            if (tmp.value == value) {
+                return true;
+            }
+            tmp = tmp.next;
+        }
+        return false;
+    }
+    // returns the index of the node containing value or null if not found 
+    const find = function (value) {
+        let index = 0;
+        let tmp = this.head;
+        while (tmp !== null) {
+            if (tmp.value == value) {
+                return index;
+            }
+            tmp = tmp.next;
+            index++;
+        }
+        return null;
+    }
+    //represent the linked list objects as strings 
+    const toString = function () {
+        let linkedList = [];
+        let tmp = this.head;
+        while (tmp !== null) {
+            linkedList.push(`(${tmp.value})->`);
+            tmp = tmp.next;
+        }
+        return linkedList;
+    }
 
-    return { head : null, append, prepend };
+    return { head: null, append, prepend, size, findHead, findTail, at, pop, contains, find, toString };
 }
 
 
@@ -41,5 +111,16 @@ linkedListOne.append(5);
 linkedListOne.append(10);
 linkedListOne.prepend(12);
 linkedListOne.prepend(13);
+linkedListOne.prepend(16);
+console.log(linkedListOne.size());
+console.log(linkedListOne.findHead());
+console.log(linkedListOne.findTail());
+console.log(linkedListOne.at(1));
+console.log(linkedListOne.pop());
+console.log(linkedListOne.contains(12));
+console.log(linkedListOne.contains(20));
+console.log(linkedListOne.find(20));
+console.log(linkedListOne.find(13));
+console.log(linkedListOne.toString());
 
 console.log(linkedListOne);
