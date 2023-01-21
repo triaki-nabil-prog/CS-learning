4a11d49ec6827f516999f5bea1c9c6c4da99661d
triaki-nabil-prog <nabil.triaki2@gmail.com>
2023-01-21 10:59:26 +0100

    created a method that add new node to leaf of a already existing tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,5 +1,5 @@
 // a binary tree node 
-const node = function newNodeCreation(data) {
+const node = function rootCreation(data) {
     return {
         data,
         left: null,
@@ -8,15 +8,41 @@ const node = function newNodeCreation(data) {
 }
 // root of the binary tree
 const tree = function (array) {
-    let sortedArray = [...new Set(array.sort(function (a, b) { return b - a }))];
+    //insert a new node in the binary tree 
+    const insert = function (value, root = this.root) {
+        //if tree empty add new node to root
+        if (root === null) {
+            root = node(value);
+            return root;
+        }
+        //else traverse down the tree to the leaf 
+        if (root.data > value) {
+            console.log(root);
+            root.left = insert(value, root.left);
+        }
+        else if (root.data < value) {
+            console.log(root);
+            root.right = insert(value, root.right);
+        }
+        return root;
+    }
+    // delete a specific node from the binary tree
+    const remove = function (value) {
+    }
+    //sort and delete duplicated values 
+    let sortedArray = [...new Set(array.sort(function (a, b) { return a - b }))];
     return {
-        root: buildTree(sortedArray,0,sortedArray.length-1)
+        root: buildTree(sortedArray, 0, sortedArray.length - 1),
+        insert,
+        remove,
     };
-}
+};
+
+
 //  function to create a balanced binary tree from sorted array 
 const buildTree = function (array, start, end) {
     //base case to stope recursion 
-    if (start > end) {return null;}
+    if (start > end) { return null; }
     //get the middle element
     let mid = Math.floor((start + end) / 2);
     // make it into a node 
@@ -27,6 +53,12 @@ const buildTree = function (array, start, end) {
     Node.right = buildTree(array, mid + 1, end);
     return Node;
 }
+
+
+
+
+
+
 //binary search tree visualization 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
@@ -38,7 +70,9 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
-let newTree=tree([5,5,5,8,7,4,6,9,8,7,6,5]);
+let newTree = tree([5, 5, 5, 8, 7, 4, 6, 9, 8, 7, 6, 5]);
+
 console.log(newTree);
+newTree.insert(10);
 prettyPrint(newTree.root);
 
